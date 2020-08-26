f5a4ad9ed41079e249a5ca62a8dc9ed4acf9a55f
Don1011 <66220736+Don1011@users.noreply.github.com>
2020-08-26 23:04:59 +0100

    Update topic_details.php

--- topic_details.php
+++ topic_details.php
@@ -6,7 +6,47 @@
         
         <div class="container page_body">
             <?php
-                if(isset($_GET['topic_id'])){
+                if(isset($_GET['topic_id']) && isset($_POST['name']) && isset($_POST['matric_number']) && isset($_POST['password']) && isset($_POST['confirm_password'])){
+                    
+                    $topic_id = $_GET['topic_id'];
+
+                    if(!empty($_GET['topic_id']) && !empty($_POST['name']) && !empty($_POST['matric_number']) && !empty($_POST['password']) && !empty($_POST['confirm_password'])){
+                        
+                        $name = $_POST['name'];
+                        $matric_number = $_POST['matric_number'];
+                        $password = $_POST['password'];
+                        $confirm_password = $_POST['confirm_password'];
+                        $time = date('h:i:s');
+                        $date = date('Y-m-d');
+
+                        if($password == $confirm_password){
+                            $sql_reg = "INSERT INTO topic_registration(`student_name`, `student_matric_number`, `password`, `topic_id`, `registration_time`, `registration_date` ) VALUES('$name', '$matric_number', '$password', '$topic_id', '$time', '$date')";
+
+                            $query_reg = mysqli_query($conn, $sql_reg);
+                             
+                            echo "<script lang = 'javascript'>
+                                alert('Topic Registration Successful.');
+                                location.href = 'topic_details.php?topic_id=$topic_id';
+                            </script>";
+                            // header("location: topic_details.php?topic_id=$topic_id");
+                        }else{
+                            echo "<script lang = 'javascript'>
+                                alert('Make sure passwords match before submitting the form.');
+                                location.href = 'topic_details.php?topic_id=$topic_id';
+                            </script>";
+                            // header("location: topic_details.php?topic_id=$topic_id");
+                        }
+
+                    }else{
+                        echo "<script lang = 'javascript'>
+                            alert('Complete filling the form before submitting.');
+                            location.href = 'topic_details.php?topic_id=$topic_id';
+                        </script>";
+                        // header("location: topic_details.php?topic_id=$topic_id");
+                    }
+                    
+                }elseif(isset($_GET['topic_id'])){
+                    
                     $topic_id = $_GET['topic_id'];
 
                     $sql_topic = "SELECT * FROM project_topic WHERE id = '$topic_id'";
@@ -62,7 +102,58 @@
                                 endwhile;
                             ?>
                         </div>
-                        
+
+
+
+
+
+                        <div class="bootstrap-modal my-5 text-center">
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#exampleModalLong">
+                                Register Topic
+                            </button>
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModalLong">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title">Fill in your details.</h5>
+                                            <button type="button" class="close" data-dismiss="modal"><span>&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form class = "text-left" action="topic_details.php?topic_id=<?php echo $_GET['topic_id']?>" method = "POST">
+                                                <div class="form-group">
+                                                    <label for="name">Full Name</label>
+                                                    <input type="text" id = "name" name = "name" class="form-control">
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label for="matric_number">Matric Number</label>
+                                                    <input type="text" id = "matric_number" name = "matric_number" class="form-control">
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label for="password">Password</label>
+                                                    <input type="password" id = "password" name = "password" class="form-control">
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label for="confirm_password">Confrim Password</label>
+                                                    <input type="password" id = "confirm_password" name = "confirm_password" class="form-control">
+                                                </div>
+
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                    <button type="submit" class="btn btn-primary">Register</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 
